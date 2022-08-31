ac334d1b588b36616f1841a9d6bea6fe71cdff58
juansob <juanjosob3@gmail.com>
2022-08-31 15:47:02 -0600

    ¿Quienes Somos? terminado

--- web-facultad-icc/src/componentes/paginas/QuienesSomos.jsx
+++ web-facultad-icc/src/componentes/paginas/QuienesSomos.jsx
@@ -1,5 +1,8 @@
 import React from 'react';
 import Carousel from 'react-bootstrap/Carousel';
+import '../../stylesheet/quienesSomos.css';
+import Col from 'react-bootstrap/Col';
+import Row from 'react-bootstrap/Row';
 
 function QuienesSomos({imagenNav, carousel1, carousel2})
 {
@@ -32,6 +35,50 @@ function QuienesSomos({imagenNav, carousel1, carousel2})
                     alt="Logo de la Facultad"
                 />
             </div>
+            <hr className='linea'/>
+            <h1 className='text-center text-uppercase titulo'>Quiénes somos</h1>
+            <p className='justify-content text-uppercase infoqs'>
+                Somos una carrera en donde se abordan las bases teóricas y metodológicas necesarias para el diseño, 
+                implantación, análisis, control, procesamiento, transporte, operatividad, toma de decisiones y búsqueda de seguridad 
+                de los sistemas informáticos.
+                <br />
+                Nuestro enfoque está en estudiar el desarrollo de sistemas automatizados y el uso de los lenguajes de programación; 
+                de igual forma se enfoca al análisis, diseño y la utilización del hardware y software para lograr la implementación 
+                de las más avanzadas aplicaciones industriales y telemáticas
+            </p>
+            <hr className='linea'/>
+            <Row className='text-center'>
+                <Col className='text-uppercase'>
+                    <h2>Misión</h2>
+                    <p className='mv justify-text'>
+                        En el año 2022 seremos una universidad donde lo espiritual es tan importante como lo académico, con un sistema certificado y acreditado de educación superior, para el desarrollo intelectual, ético y moral de profesionales que contribuyan a la construcción de una nueva sociedad.
+                    </p>
+                </Col>
+                <Col className='text-uppercase'>
+                    <h2>Visión</h2>
+                    <p className='mv justify-text'>
+                        Garantizar la presencia del catolicismo en la educación superior de Honduras y asegurar una formación integral en función de las distintas dimensiones del ser humano: lo social, lo moral, lo espiritual, lo científico, lo técnico y lo religioso.
+                    </p>
+                </Col>
+            </Row>
+            <h2 className='text-center text-uppercase valoresTitulo'>Nuestros Valores</h2>
+            <Row className='text-uppercase text-center valores'>
+                <Col>
+                    <li>Justicia</li>
+                </Col>
+                <Col>
+                    <li>Orden</li>
+                </Col>
+                <Col>
+                    <li>Paz</li>
+                </Col>
+                <Col>
+                    <li>Amor</li>
+                </Col>
+                <Col>
+                    <li>Verdad</li>
+                </Col>
+            </Row>
         </>
     );
 }
