6961f0e915958acf84ec7fcc1335f8680a0cfc08
juansob <juanjosob3@gmail.com>
2022-09-05 22:25:42 -0600

    Contacto correo terminado

--- web-facultad-icc/src/componentes/paginas/Contacto.jsx
+++ web-facultad-icc/src/componentes/paginas/Contacto.jsx
@@ -1,16 +1,59 @@
-import React from "react";
+import React, { useRef } from "react";
+import emailjs from '@emailjs/browser';
+import { Row, Col } from "react-bootstrap";
+import '../../stylesheet/contacto.css';
+import Form from 'react-bootstrap/Form';
 
 export function Contacto({imagenNav})
 {
+    const sendEmail = (e) => {
+        e.preventDefault();
+
+        emailjs.sendForm('service_3jl48qm', 'template_2tlxwll', e.target, 'm-_ulqYjjv5In9T_B')
+        .then((result) => {
+            console.log(result.text);
+            e.target.reset();
+        }, (error) => {
+            console.log(error.text);
+        });
+    };
     return (
-        <div className='Contenido'>
-            <img
-                className="rutaImagenBajo"
-                src={require(`../../assets/img/navegacion/${imagenNav}`)}
-                alt="Adorno Nav"
-            />
-            <h1>Contacto</h1>
-        </div>
+        <>
+            <div className='Contenido'>
+                <img
+                    className="rutaImagenBajo"
+                    src={require(`../../assets/img/navegacion/${imagenNav}`)}
+                    alt="Adorno Nav"
+                />
+            </div>
+            <h1 className='text-center text-uppercase'>Ponte en contacto con nosotros:</h1>
+            <p className='justify-content text-center text-uppercase'>No olvides llenar cada uno de los campos.🤠</p>
+            <Form className="text-center text-uppercase" onSubmit={sendEmail}>
+                <Row className="row pt-5 mx-auto">
+                    <Col className="col-8 form-group pt-2 mx-auto">
+                        <Form.Label className="labelControl">Tema de mensaje</Form.Label>
+                        <Form.Control className="text-center form-control-lg" type="text" name="subject" placeholder="Consulta Actividad"/>
+                    </Col>
+                    <Col className="col-8 form-group pt-2 mx-auto">
+                        <Form.Label className="labelControl">Nombre</Form.Label>
+                        <Form.Control className="text-center form-control-lg" type="text" name="name" placeholder="Juan Sobalvarro"/>
+                    </Col>
+                    <Col className="col-8 form-group pt-2 mx-auto">
+                        <Form.Label className="labelControl">Correo Electrónico</Form.Label>
+                        <Form.Control className="text-center form-control-lg" type="email" name="email" placeholder="ejemplo@unicah.edu"/>
+                    </Col>
+                    <Col className="col-8 form-group pt-2 mx-auto">
+                        <Form.Label className="labelControl">Mensaje</Form.Label>
+                        <Form.Control as="textarea" rows={3} name="message" />
+                    </Col>
+                    <div className="button_container">
+                        <button className="btn boton" type="submit" value="Send">
+                            <span>Enviar Correo</span>
+                        </button>
+                    </div>
+                </Row>
+            </Form>
+        </>
     );
 }
 
--- web-facultad-icc/src/componentes/paginas/QuienesSomos.jsx
+++ web-facultad-icc/src/componentes/paginas/QuienesSomos.jsx
@@ -6,30 +6,29 @@ import Row from 'react-bootstrap/Row';
 import ReactPlayer from 'react-player/lazy';
 import AOS from 'aos';
 
-function QuienesSomos({imagenNav, carousel1, carousel2})
-{
-    
+function QuienesSomos({ imagenNav, carousel1, carousel2 }) {
+
     return (
         <>
             <Carousel fade>
-                    <Carousel.Item interval={5000}>
-                        <a href="https://unicah.edu/FlujogramasWeb/#IF01002">
-                            <img
+                <Carousel.Item interval={5000}>
+                    <a href="https://unicah.edu/FlujogramasWeb/#IF01002">
+                        <img
                             className="d-block w-100"
                             src={require(`../../assets/img/quienesSomos/${carousel1}`)}
                             alt="Plan de estudio"
-                            />
-                        </a>
-                    </Carousel.Item>
-                    <Carousel.Item interval={5000}>
-                        <a href="https://drive.google.com/file/d/1vzmz3Ri1vnhkcTa54z6M7C2ApJ8MpULf/view?usp=sharing">
-                            <img
+                        />
+                    </a>
+                </Carousel.Item>
+                <Carousel.Item interval={5000}>
+                    <a href="https://drive.google.com/file/d/1vzmz3Ri1vnhkcTa54z6M7C2ApJ8MpULf/view?usp=sharing">
+                        <img
                             className="d-block w-100"
                             src={require(`../../assets/img/quienesSomos/${carousel2}`)}
                             alt="Horario Academico"
-                            />
-                        </a>
-                    </Carousel.Item>
+                        />
+                    </a>
+                </Carousel.Item>
             </Carousel>
             <div className='Contenido'>
                 <img
@@ -38,47 +37,47 @@ function QuienesSomos({imagenNav, carousel1, carousel2})
                     alt="Logo de la Facultad"
                 />
             </div>
-            <hr className='linea'   
+            <hr className='linea'
                 data-aos="fade-up"
                 data-aos-easing="linear"
-                data-aos-duration="1500"/>
+                data-aos-duration="1500" />
 
-            <h1 className='text-center text-uppercase titulo' 
+            <h1 className='text-center text-uppercase titulo'
                 data-aos="fade-up"
                 data-aos-easing="linear"
                 data-aos-duration="1500">Quiénes somos</h1>
-            <p className='justify-content text-uppercase infoqs' 
-               data-aos="fade-up"
-               data-aos-easing="linear"
-               data-aos-duration="1500">
-                Somos una carrera en donde se abordan las bases teóricas y metodológicas necesarias para el diseño, 
-                implantación, análisis, control, procesamiento, transporte, operatividad, toma de decisiones y búsqueda de seguridad 
+            <p className='justify-content text-uppercase infoqs'
+                data-aos="fade-up"
+                data-aos-easing="linear"
+                data-aos-duration="1500">
+                Somos una carrera en donde se abordan las bases teóricas y metodológicas necesarias para el diseño,
+                implantación, análisis, control, procesamiento, transporte, operatividad, toma de decisiones y búsqueda de seguridad
                 de los sistemas informáticos.
                 <br />
-                Nuestro enfoque está en estudiar el desarrollo de sistemas automatizados y el uso de los lenguajes de programación; 
-                de igual forma se enfoca al análisis, diseño y la utilización del hardware y software para lograr la implementación 
+                Nuestro enfoque está en estudiar el desarrollo de sistemas automatizados y el uso de los lenguajes de programación;
+                de igual forma se enfoca al análisis, diseño y la utilización del hardware y software para lograr la implementación
                 de las más avanzadas aplicaciones industriales y telemáticas
             </p>
 
-            <hr className='linea'   
+            <hr className='linea'
                 data-aos="fade-up"
                 data-aos-easing="linear"
-                data-aos-duration="1500"/>
+                data-aos-duration="1500" />
 
             <Row className='text-center'>
-                <Col className='text-uppercase' 
-                     data-aos="fade-right"
-                     data-aos-easing="linear"
-                     data-aos-duration="1500">
+                <Col className='text-uppercase'
+                    data-aos="fade-right"
+                    data-aos-easing="linear"
+                    data-aos-duration="1500">
                     <h2 className='tituloVM'>Misión</h2>
                     <p className='mv justify-text'>
                         En el año 2022 seremos una universidad donde lo espiritual es tan importante como lo académico, con un sistema certificado y acreditado de educación superior, para el desarrollo intelectual, ético y moral de profesionales que contribuyan a la construcción de una nueva sociedad.
                     </p>
                 </Col>
-                <Col className='text-uppercase' 
-                     data-aos="fade-left"
-                     data-aos-easing="linear"
-                     data-aos-duration="1500">
+                <Col className='text-uppercase'
+                    data-aos="fade-left"
+                    data-aos-easing="linear"
+                    data-aos-duration="1500">
 
                     <h2 className='tituloVM'>Visión</h2>
                     <p className='mv justify-text'>
@@ -86,15 +85,15 @@ function QuienesSomos({imagenNav, carousel1, carousel2})
                     </p>
                 </Col>
             </Row>
-            <h2 className='text-center text-uppercase valoresTitulo' 
+            <h2 className='text-center text-uppercase valoresTitulo'
                 data-aos="fade-up"
                 data-aos-easing="linear"
                 data-aos-duration="1500">Nuestros Valores</h2>
 
-            <Row className='text-uppercase text-center valores' 
-                 data-aos="fade-up"
-                 data-aos-easing="linear"
-                 data-aos-duration="1500">
+            <Row className='text-uppercase text-center valores'
+                data-aos="fade-up"
+                data-aos-easing="linear"
+                data-aos-duration="1500">
                 <Col>
                     Justicia
                 </Col>
@@ -112,22 +111,22 @@ function QuienesSomos({imagenNav, carousel1, carousel2})
                 </Col>
             </Row>
 
-            <hr className='linea'   
+            <hr className='linea'
                 data-aos="fade-up"
                 data-aos-easing="linear"
-                data-aos-duration="1500"/>
+                data-aos-duration="1500" />
 
-            <h2 className='text-center text-uppercase tituloVideo' 
+            <h2 className='text-center text-uppercase tituloVideo'
                 data-aos="fade-up"
                 data-aos-easing="linear"
                 data-aos-duration="1500">Conoce un poco más sobre nosotros</h2>
 
-            <div className='player-wrapper scroll-arreglado-ejeY' 
-                    data-aos="fade-right"
-                    data-aos-easing="linear"
-                    data-aos-duration="1500">
+            <div className='player-wrapper scroll-arreglado-ejeY'
+                data-aos="fade-right"
+                data-aos-easing="linear"
+                data-aos-duration="1500">
 
-                <ReactPlayer 
+                <ReactPlayer
                     url='https://youtu.be/QQTKvPbJ1YM'
                     controls='true'
                     width='100%'
