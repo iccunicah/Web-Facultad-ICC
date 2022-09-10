55ca3699aa1b0c5a85ba7c97c7724c77ad8a0435
juansob <juanjosob3@gmail.com>
2022-09-09 22:06:45 -0600

    Validación correo

--- web-facultad-icc/src/componentes/paginas/Contacto.jsx
+++ web-facultad-icc/src/componentes/paginas/Contacto.jsx
@@ -1,9 +1,11 @@
-import React, { useRef } from "react";
+import React, { useRef, useState } from "react";
 import emailjs from '@emailjs/browser';
-import { Row, Col } from "react-bootstrap";
+import { Row, Col, Alert } from "react-bootstrap";
 import '../../stylesheet/contacto.css';
 import Form from 'react-bootstrap/Form';
 import Card from 'react-bootstrap/Card';
+import { useFormik } from 'formik';
+import * as Yup from "yup";
 
 //Iconos
 import CorporateFareIcon from '@mui/icons-material/CorporateFare';
@@ -21,15 +23,48 @@ import PublicIcon from '@mui/icons-material/Public';
 
 
 export function Contacto({ imagenNav }) {
-    const sendEmail = (e) => {
-        e.preventDefault();
+    let envio=true;
+    let msj="";
+    const formik = useFormik(
+        {initialValues: {
+            subject:"",
+            name:"",
+            email:"",
+            message:"",
+        },
+        validationSchema: Yup.object({
+            subject: Yup.string().max(20,'Este campo no admite mas de 20 caractéres.').required('Este campo es obligatorio.'),
+            name: Yup.string().matches(/^[a-zA-Z\s]*$/, 'En este campo solamente se pueden incluir letras.').required('Este campo es obligatorio.'),
+            email: Yup.string().email('En este campo solo puede añadir su correo electrónico.').required('Este campo es obligatorio.'),
+            message: Yup.string().required('Este campo es obligatorio.'),
+        }),
+        onSubmit:(values)=>{
+            console.log(values);
+            sendEmail(values.subject, values.name, values.email, values.message);
+            values.subject="";
+            values.message="";
+            values.name="";
+            values.email="";
+        }
+    });
 
-        emailjs.sendForm('service_3jl48qm', 'template_2tlxwll', e.target, 'm-_ulqYjjv5In9T_B')
+    const sendEmail = (subject, name, email, message) => {
+        const templateParams = {
+            subject: subject, 
+            name: name,
+            email: email, 
+            message: message
+        }
+
+        emailjs.sendForm('service_3jl48qm', 'template_2tlxwll', templateParams, 'm-_ulqYjjv5In9T_B')
             .then((result) => {
                 console.log(result.text);
-                e.target.reset();
+                envio= true;
+                msj = "Se ha enviado el correo correctamente"
             }, (error) => {
                 console.log(error.text);
+                envio= false;
+                msj = "No se ha podido enviar el correo."
             });
     };
     return (
@@ -159,31 +194,38 @@ export function Contacto({ imagenNav }) {
             <hr className='linea'/>
             <h1 className='text-center text-uppercase m-5'>Ponte en contacto con nosotros:</h1>
             <p className='justify-content text-center text-uppercase'>No olvides llenar cada uno de los campos.</p>
-            <Form className="text-center text-uppercase" onSubmit={sendEmail}>
-                <Row className="row pt-5 mx-auto">
-                    <Col className="col-8 form-group pt-2 mx-auto">
-                        <Form.Label className="labelControl">Tema de mensaje</Form.Label>
-                        <Form.Control className="text-center form-control-lg" type="text" name="subject" placeholder="Consulta Actividad" />
-                    </Col>
-                    <Col className="col-8 form-group pt-2 mx-auto">
-                        <Form.Label className="labelControl">Nombre</Form.Label>
-                        <Form.Control className="text-center form-control-lg" type="text" name="name" placeholder="Juan Sobalvarro" />
-                    </Col>
-                    <Col className="col-8 form-group pt-2 mx-auto">
-                        <Form.Label className="labelControl">Correo Electrónico</Form.Label>
-                        <Form.Control className="text-center form-control-lg" type="email" name="email" placeholder="ejemplo@unicah.edu" />
-                    </Col>
-                    <Col className="col-8 form-group pt-2 mx-auto">
-                        <Form.Label className="labelControl">Mensaje</Form.Label>
-                        <Form.Control as="textarea" rows={3} name="message" />
-                    </Col>
-                    <div className="button_container">
-                        <button className="btn boton" type="submit" value="Send">
-                            <span>Enviar Correo</span>
-                        </button>
-                    </div>
-                </Row>
-            </Form>
+            <div className='input-container'>
+                <Form className="text-center text-uppercase" onSubmit={formik.handleSubmit}>
+                    <Row className="row pt-5 mx-auto">
+                        <Col className="col-8 form-group pt-2 mx-auto">
+                            <Form.Label className="labelControl">Tema de mensaje</Form.Label>
+                            <Form.Control className="text-center form-control-lg" type="text" name="subject" placeholder="Consulta Actividad" onChange={formik.handleChange} value={formik.values.subject}/>
+                            {formik.errors.subject ? <p className="text-danger">{formik.errors.subject}</p> : null}
+                        </Col>
+                        <Col className="col-8 form-group pt-2 mx-auto">
+                            <Form.Label className="labelControl">Nombre</Form.Label>
+                            <Form.Control className="text-center form-control-lg" type="text" name="name" placeholder="Juan Sobalvarro" onChange={formik.handleChange}  value={formik.values.name}/>
+                            {formik.errors.name ? <p className="text-danger">{formik.errors.name}</p> : null}
+                        </Col>
+                        <Col className="col-8 form-group pt-2 mx-auto">
+                            <Form.Label className="labelControl">Correo Electrónico</Form.Label>
+                            <Form.Control className="text-center form-control-lg" type="text" name="email" placeholder="ejemplo@unicah.edu" onChange={formik.handleChange}  value={formik.values.email}/>
+                            {formik.errors.email ? <p className="text-danger">{formik.errors.email}</p> : null}
+                        </Col>
+                        <Col className="col-8 form-group pt-2 mx-auto">
+                            <Form.Label className="labelControl">Mensaje</Form.Label>
+                            <Form.Control as="textarea" rows={3} name="message" onChange={formik.handleChange}  value={formik.values.message}/>
+                            {formik.errors.message ? <p className="text-danger">{formik.errors.message}</p> : null}
+                        </Col>
+                        <div className="button_container">
+                            <button className="btn boton" type="submit" value="Send">
+                                <span>Enviar Correo</span>
+                            </button>
+                        </div>
+                    </Row>
+                    {envio ? <Alert variant="succes">{msj}</Alert> : <Alert variant="danger">{msj}</Alert>}
+                </Form>
+            </div>
         </>
     );
 }
